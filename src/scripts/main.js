08ada599b286392b9dd4d20469d4627bf97bf3b3
Tim Perry <tim.perry@softwire.com>
2014-01-21 22:37:45 +0000

    Add all the code for the presentation to the presentation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,10 +6,11 @@ var deck = bespoke.from('article', {
   state: true
 });
 
+var server = "http://localhost:8001";
+
 deck.on('activate', function (event) {
-  var slideData = $(event.slide).data();
+  var slideData = $(event.slide).data();  
   if (slideData["controlUrl"]) {
-    console.log("Hit " + slideData["controlUrl"]);
-    $.get("http://localhost:8001" + slideData["controlUrl"]);
+    $.get(server + slideData["controlUrl"]);
   }
 });
